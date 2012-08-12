fb242fd750e4f682a4c3ef4ae74b0852874a554c
Tuan-Tu TRAN <tuantu.t@gmail.com>
2012-08-12 04:42:31 +0200

    ring based ellipsoid

--- ellipsoid.R
+++ ellipsoid.R
@@ -1,4 +1,6 @@
 ellipsoid<-function(
+	hx=10,
+	hrot=15,
 	h=100,
 	a=1,
 	b=1,
@@ -10,7 +12,8 @@ ellipsoid<-function(
 	...
 )
 {
-	x=seq(-a,a,2*a/h);
+	x=seq(-a,a,2*a/hx);
+	rot=c(seq(0,pi,pi/hrot),seq(pi,2*pi,pi/hrot));
 	y=seq(-b,b,2*b/h);
 	if(zlim==-1)
 	{
@@ -19,34 +22,33 @@ ellipsoid<-function(
 	lenx=length(x);
 	leny=length(y);
 	z=matrix(nrow=lenx, ncol=leny);
+	lenrot=length(rot);
 	pmat=persp(x,y,z,theta=theta,phi=phi,zlim=zlim,scale=scale,...);
-	ptz=c();
-	ptx=c();
-	pty=c();
 	for(i in 1:lenx)
 	{
-		for(j in 1:leny)
+		X=x[i];
+		Ry=abs(b*sqrt(a^2-X^2)/a);
+		Rz=abs(c*sqrt(a^2-X^2)/a);
+		ptz=c();
+		ptx=c();
+		pty=c();
+		for(j in 1:lenrot)
 		{
-			d=a^2*b^2-b^2*x[i]^2-a^2*y[j]^2;
-			if(d>=0)
-			{
-				z[i,j]=abs(c/a/b)*sqrt(d);
-				ptz<-c(ptz,-z[i,j]);
-				ptx<-c(ptx,x[i]);
-				pty<-c(pty,y[j]);
+			alpha=rot[j];
+			Y=Ry*cos(alpha);
+			Z=Rz*sin(alpha);
 
-				ptz<-c(ptz,z[i,j]);
-				ptx<-c(ptx,x[i]);
-				pty<-c(pty,y[j]);
-			}
-			else
-			{
-				z[i,j]=NA;
-			}
+			ptz<-c(ptz,Z);
+			ptx<-c(ptx,X);
+			pty<-c(pty,Y);
 		}
+		res=trans3d(ptx,pty,ptz,pmat);
+		points(
+			res$x,
+			res$y,
+			type="l"
+		);
 	}
-	res=trans3d(ptx,pty,ptz,pmat);
-	points(res$x,res$y,pch=".");
 }
 
 res=ellipsoid();
